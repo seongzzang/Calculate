f01eac9316b2f94d8db6e6a8307612205d9490cb
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-07-19 18:11:33 +0900

    isRound

---
+++ Calculate/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  Calculate
+//
+//  Created by 양성혜 on 2022/07/19.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+
+}
